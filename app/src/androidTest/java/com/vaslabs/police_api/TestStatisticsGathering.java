1743b808de1485ae0d70908bd5e316327d6d6239
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-11-03 19:03:32 +0000

    Get crime entries by the nearest to you

--- app/src/androidTest/java/com/vaslabs/police_api/TestStatisticsGathering.java
+++ app/src/androidTest/java/com/vaslabs/police_api/TestStatisticsGathering.java
@@ -2,6 +2,7 @@ package com.vaslabs.police_api;
 
 import android.test.AndroidTestCase;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import com.vaslabs.guardianbuddy.R;
 
@@ -15,7 +16,6 @@ import java.io.Reader;
 public class TestStatisticsGathering extends AndroidTestCase{
 
     public void testStatsFromCrimeEntries() {
-        Gson gson = new Gson();
         Reader reader = new BufferedReader(new InputStreamReader(this.getContext().getResources().openRawResource(R.raw.sample_data)));
         CrimeEntry[] crimeEntries = CrimeEntriesService.getCrimeEntriesFromJson(reader);
         CrimeStatistics crimeStatistics = CrimeStatistics.generate(crimeEntries);
@@ -23,6 +23,21 @@ public class TestStatisticsGathering extends AndroidTestCase{
         assertEquals(107, crimeStatistics.getByCategory(CategoryType.ANTISOCIAL));
         assertEquals(5, crimeStatistics.getByCategory(CategoryType.DRUGS));
         assertEquals(0, crimeStatistics.getByCategory(CategoryType.UNIDENTIFIED));
+    }
+
+    public void testGetCrimesSortedByNearestToMe() {
+        LatLng myPosition = new LatLng(53.472225, -2.2936317);
+        Reader reader = new BufferedReader(new InputStreamReader(this.getContext().getResources().openRawResource(R.raw.sample_data)));
+        CrimeEntry[] crimeEntries = CrimeEntriesService.getCrimeEntriesFromJson(reader);
+        CrimeStatistics crimeStatistics = CrimeStatistics.generate(crimeEntries);
+        crimeEntries = crimeStatistics.orderByNearest(myPosition);
+        float prevDistance = CrimeStatistics.distanceBetween(crimeEntries[0], myPosition);
+        for (int i = 1; i < crimeEntries.length; i++) {
+            final float currentDistance = CrimeStatistics.distanceBetween(crimeEntries[1], myPosition);
+            assertTrue(currentDistance >= prevDistance);
+            prevDistance = currentDistance;
+        }
+
     }
 
 }
--- app/src/main/java/com/vaslabs/police_api/CrimeStatistics.java
+++ app/src/main/java/com/vaslabs/police_api/CrimeStatistics.java
@@ -1,21 +1,58 @@
 package com.vaslabs.police_api;
 
+import android.location.*;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by vnicolaou on 03/11/15.
  */
 public class CrimeStatistics {
 
     private int[] statsByCategory = new int[CategoryType.values().length];
-
+    private CrimeEntry[] crimeEntries;
     public static CrimeStatistics generate(CrimeEntry[] crimeEntries) {
         CrimeStatistics crimeStatistics = new CrimeStatistics();
         for (CrimeEntry crimeEntry : crimeEntries) {
             crimeStatistics.statsByCategory[crimeEntry.getCategory().ordinal()]++;
         }
+        crimeStatistics.crimeEntries = Arrays.copyOf(crimeEntries, crimeEntries.length);
         return crimeStatistics;
     }
 
     public int getByCategory(CategoryType categoryType) {
         return statsByCategory[categoryType.ordinal()];
+    }
+
+    public CrimeEntry[] orderByNearest(final LatLng position) {
+        Arrays.sort(crimeEntries, new Comparator<CrimeEntry>() {
+            @Override
+            public int compare(CrimeEntry lhs, CrimeEntry rhs) {
+                float resultLhs = distanceBetween(lhs, position);
+                float resultRhs = distanceBetween(rhs, position);
+
+                float diff = resultLhs - resultRhs;
+                if (diff < 0) {
+                    return -1;
+                } else if (diff > 0)
+                    return 1;
+                else
+                    return 0;
+            }
+        });
+        return crimeEntries;
+    }
+
+    public static float distanceBetween(CrimeEntry crimeEntry, LatLng position) {
+        double ceLatitude = crimeEntry.getLocation().getLatitude();
+        double ceLongitude = crimeEntry.getLocation().getLongitude();
+        double centerLatitude = position.latitude;
+        double centerLongitude = position.longitude;
+        float[] result = new float[1];
+        android.location.Location.distanceBetween(centerLatitude, centerLongitude, ceLatitude, ceLongitude, result);
+        return result[0];
     }
 }
