7567d64c1f5720954a3e1fe6c7b46f647273161e
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-11-03 20:32:34 +0000

    Scan and map crime entries in your area

--- app/src/main/java/com/vaslabs/guardianbuddy/MapActivity.java
+++ app/src/main/java/com/vaslabs/guardianbuddy/MapActivity.java
@@ -1,18 +1,43 @@
 package com.vaslabs.guardianbuddy;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.vaslabs.police_api.CrimeEntriesService;
+import com.vaslabs.police_api.CrimeEntry;
+import com.vaslabs.police_api.CrimeStatistics;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private static final String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
+    private Marker myPositionMarker;
+    private List<Marker> crimeMarkers = new ArrayList<Marker>();
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +47,64 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        CrimeEntriesService.init(this);
+        context = this;
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map_session, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.show_crime) {
+            showCrime();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showCrime() {
+        CrimeEntriesService.getInstance().getCrimeEntriesAround(myPositionMarker.getPosition(),
+                "2015-07", this, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        handleResponse(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+
+    }
+
+    private void handleResponse(String response) {
+        CrimeEntry[] crimeEntries = CrimeEntriesService.getCrimeEntriesFromJson(response);
+        CrimeStatistics crimeStatistics = CrimeStatistics.generate(crimeEntries);
+        crimeEntries = crimeStatistics.orderByNearest(myPositionMarker.getPosition());
+        for (int i = 0; i < crimeEntries.length && i < 10; i++) {
+            final CrimeEntry crimeEntry = crimeEntries[i];
+            final LatLng latLng = new LatLng(crimeEntry.getLocation().getLatitude(),
+                    crimeEntry.getLocation().getLongitude());
+
+            Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title(crimeEntry.getCategory().name())
+                    .icon(BitmapDescriptorFactory.defaultMarker(crimeEntry.getCategory().color)));
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +120,55 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        initLocationManager();
+        Location currentLocation = locationManager.getLastKnownLocation(Context.LOCATION_SERVICE);
+        if (currentLocation != null ) {
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+            myPositionMarker = mMap.addMarker(new MarkerOptions().position(latLng)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 12f));
+        }
+
+    }
+
+    private void initLocationManager() {
+        locationManager = (LocationManager)(this.getSystemService(Context.LOCATION_SERVICE));
+        if (locationManager == null) {
+            Toast.makeText(this, "Please enable gps", Toast.LENGTH_LONG).show();
+            return;
+        }
+        locationManager.requestLocationUpdates(LOCATION_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        if (myPositionMarker == null) {
+            myPositionMarker = mMap.addMarker(new MarkerOptions().position(latLng)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        } else {
+            myPositionMarker.setPosition(latLng);
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12f));
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Please enable gps", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/vaslabs/police_api/CategoryType.java
+++ app/src/main/java/com/vaslabs/police_api/CategoryType.java
@@ -2,21 +2,30 @@ package com.vaslabs.police_api;
 
 import android.util.Log;
 
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
 /**
  * Created by vnicolaou on 03/11/15.
  */
 public enum CategoryType {
     ANTISOCIAL("anti-social-behaviour"), BICYCLE_THEFT("bicycle-theft"), BURGLARY("burglary"), CRIMINAL_DAMAGE_ARSON("criminal-damage-arson"),
-    DRUGS("drugs"), OTHER_THEFT("other-theft"), SHOPLIFTING("shoplifting"), THEFT_FROM_PERSON("theft-from-the-person"), VEHICLE_CRIME("vehicle-crime"),
-    VIOLENT_CRIME("violent-crime"), ROBBERY("robbery"), OTHER_CRIME("other-crime"), POSSESSION_OF_WEAPONS("possession-of-weapons"),
+    DRUGS("drugs"), OTHER_THEFT("other-theft"), SHOPLIFTING("shoplifting"), THEFT_FROM_PERSON("theft-from-the-person"),
+    VEHICLE_CRIME("vehicle-crime", BitmapDescriptorFactory.HUE_ORANGE),
+    VIOLENT_CRIME("violent-crime", BitmapDescriptorFactory.HUE_RED), ROBBERY("robbery"),
+    OTHER_CRIME("other-crime"), POSSESSION_OF_WEAPONS("possession-of-weapons"),
     UNIDENTIFIED("other"), LOCAL_RESOLUTION("Local resolution"), PUBLIC_ORDER("public-order");
 
     private final String description;
+    public float color;
 
     CategoryType(String description) {
-        this.description = description;
+        this(description, BitmapDescriptorFactory.HUE_YELLOW);
     }
-
+    CategoryType(String description, float color) {
+        this.description = description;
+        this.color = color;
+    }
     public static CategoryType findByAbbr(String value)
     {
         for (CategoryType currEnum : CategoryType.values())
